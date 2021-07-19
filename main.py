69565468040c4fcb3bcd73aba8493958a6d3d8e5
us8 <sarafan2517@gmail.com>
2021-07-19 18:31:30 +0500

    Two files operation

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-import utils
+from utils import decor as decor
 
+if __name__ == "__main__" :
+    gen_list = [i for i in range(0,100) if i % 3 == 0]
+    print(gen_list)
+    '''
+    gen_gen = (i for i in range(0, 100) if i % 5 == 0)
+    print(gen_gen)
+    for i in gen_gen:
+        print (i)
+    '''
+    class People:
+        def __init__ (self, x,y,z):
+            self.life = x
+            self.age = y
+            self.sex = z
+
+        def __str__(self):
+           return (f'People {self.sex }, age {self.age}, value life  - {self.life}  ' )
+
+
+    Dima = People('good', 33,'male')
+    print(Dima)
+
+    @decor
+    def testing(x):
+        print(x**32)
+
+
+    testing(5)
